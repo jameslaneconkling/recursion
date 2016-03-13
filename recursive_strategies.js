e7a86f834eea9fe1f2a57ac5eaba27ac7de23d26
James Conkling <james.lane.conkling@gmail.com>
2016-03-12 23:22:33 -0500

    reformat

--- recursive_strategies.js
+++ recursive_strategies.js
@@ -1,14 +1,8 @@
 /*
- * An exploration and visualization of recursive strategies
+ * IMPURE APPROACH
  *
- *
- *
- */
-
-/*  IMPURE APPROACH
-
-    create a recursive subroutine that modifies a
-    variable in the subroutine's containing clojure
+ * create a recursive subroutine that modifies a
+ * variable in the subroutine's containing clojure
  */
 var impureRecurse = function(collection){
   var sum = 0;
@@ -24,12 +18,13 @@ var impureRecurse = function(collection){
 };
 
 
-/*  RECURSE UP
-
-    perform calculations as function returns up from the base case,
-    arriving at a solution when the top-most recursive call returns
-
-    this allows us to avoid modifying the function's arguments
+/*
+ * RECURSE UP
+ *
+ * perform calculations as function returns up from the base case,
+ * arriving at a solution when the top-most recursive call returns
+ *
+ * this allows us to avoid modifying the function's arguments
  */
 var recurseUp = function(collection){
   if(collection.length === 0){ return 0; }
@@ -37,13 +32,14 @@ var recurseUp = function(collection){
 };
 
 
-/*  RECURSE DOWN
-
-    perform calculations as function recurses downwards,
-    passing subsequent steps in the calculation as an additional argument,
-    arriving at a solution when the base case is met
-
-    this has the added benefit of being tail recursive
+/*
+ * RECURSE DOWN
+ *
+ * perform calculations as function recurses downwards,
+ * passing subsequent steps in the calculation as an additional argument,
+ * arriving at a solution when the base case is met
+ *
+ * this has the added benefit of being tail recursive
  */
 var recurseDown = function(collection, solution){
   solution = solution || 0;
@@ -51,19 +47,11 @@ var recurseDown = function(collection, solution){
   return recurseDown(collection.slice(1), solution + collection[0]);
 };
 
-// var data = [1,5,4,7,8];
-// var impure = impureRecurse(data);
-// var up = recurseUp(data);
-// var down = recurseDown(data);
 
-// console.log('impure recurse: ', impure);
-// console.log('recurse up: ', up);
-// console.log('recurse down: ', down);
-
-
-/*  DEPTH FIRST TREE RECURSE
-
-    useful for generating permutations or combinations or
+/*
+ * DEPTH FIRST TREE RECURSE
+ *
+ * useful for generating permutations or combinations or
  */
 var depthTreeRecurse = function(letters, length, word, words){
   // generate all possible word combinations of length 'length' using all characters in the 'letters' array,
@@ -84,63 +72,11 @@ var combinations = depthTreeRecurse(['a','b','c','d'], 3);
 console.log(combinations);
 
 
-/*  BREADTH FIRST TREE RECURSE
-
-    BFS is not traditionally a recursive algorithm, as it uses a queue (rather than a stack)
-    and the order functions are called is determined by the call stack
-    a recursive BFS algorithm can be implemented via Iterative deepening depth-first search:
-    https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search
+/*
+ * BREADTH FIRST TREE RECURSE
+ *
+ * BFS is not traditionally a recursive algorithm, as it uses a queue (rather than a stack)
+ * and the order functions are called is determined by the call stack
+ * a recursive BFS algorithm can be implemented via Iterative deepening depth-first search:
+ * https://en.wikipedia.org/wiki/Iterative_deepening_depth-first_search
  */
-
-
-/*  TAG
-
-    tag function invocation, resolution, inputs and outputs to map recursive structure
-    tag works by creating a proxy wrapper function that calls the input function, recording
-    that function's input and output and a unique id for each recursive call it makes to itself
- */
-var tag = function(fn){
-  var _callStack = {};
-
-  var callId = 0;
-
-  var counter = function(){ return callId++; };
-
-  var proxy = function(){
-    var input = Array.prototype.slice.apply(arguments),
-        callId = counter(),
-        output = fn.apply(callId, input);
-
-    // console.log('ID:', callId, 'arguments:', input);
-    // console.log(fn.toString());
-
-    _callStack[callId] = {
-      input: input,
-      output: output,
-      parent: null // TODO: how to pass parent id
-    };
-
-    return output;
-  };
-
-  proxy.getCallStack = function(){
-    return _callStack;
-  };
-
-  return proxy;
-};
-
-
-// down
-// var taggedRecurseDown = tag(recurseDown);
-// recurseDown = taggedRecurseDown;
-
-// taggedRecurseDown([1,5,4,7,8]);
-// console.log(taggedRecurseDown.getCallStack());
-
-
-// up
-// var taggedRecurseUp = tag(recurseUp);
-// recurseUp = taggedRecurseUp;
-
-// taggedRecurseUp([1,5,4,7,8]);
